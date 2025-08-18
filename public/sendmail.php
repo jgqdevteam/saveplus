60b549111206cfbd03decebb8dbfcfbf90c88b31
jgqdevteam <jgqdevteam@gmail.com>
2025-08-18 16:52:22 +0800

    Add files via upload

---
+++ public/sendmail.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to      = "linisplusofficial@GMAIL.COM"; // change to your email
+    $subject = "New Contact Form Message from $name";
+    $body    = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Sorry, something went wrong. Please try again.";
+    }
+}
+?>
