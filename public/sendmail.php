93390cf8db26bed0bf5d5046d3b3c05601cd70e7
jgqdevteam <jgqdevteam@gmail.com>
2025-08-19 13:28:24 +0800

    Add files via upload

--- public/sendmail.php
+++ public/sendmail.php
@@ -4,15 +4,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email   = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to      = "linisplusofficial@GMAIL.COM"; // change to your email
-    $subject = "New Contact Form Message from $name";
-    $body    = "Name: $name\nEmail: $email\n\nMessage:\n$message";
-    $headers = "From: $email";
+    $to      = "junmarkdocdoc@gmail.com"; // change to your email
+    $subject = "New Contact Form Message";
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+    
+    $body = "You received a new message:\n\n".
+            "Name: $name\n".
+            "Email: $email\n".
+            "Message:\n$message";
 
     if (mail($to, $subject, $body, $headers)) {
-        echo "Thank you! Your message has been sent.";
+        echo "Message sent successfully!";
     } else {
-        echo "Sorry, something went wrong. Please try again.";
+        echo "Failed to send message. Please try again.";
     }
+} else {
+    http_response_code(405); // Method Not Allowed
+    echo "Method not allowed";
 }
 ?>
